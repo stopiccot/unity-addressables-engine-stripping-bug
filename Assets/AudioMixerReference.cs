47ecbf0632dc6629dbe4885a4715370915375794
Alexey Petruchik <alexey.petruchik@gmail.com>
2020-06-09 14:22:25 +0300

    Addressables Engine stripping bug

---
+++ Assets/AudioMixerReference.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioMixerReference : MonoBehaviour
+{
+    [SerializeField] AudioMixer audioMixer;
+
+    private void Start()
+    {
+        audioMixer.SetFloat("test_float", 1.0f);
+    }
+}
---
+++ Assets/LoaderScript.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+
+public class LoaderScript : MonoBehaviour
+{
+    [SerializeField] AssetReference addressableScene;
+
+    public void LoadAddressableScene()
+    {
+        Addressables.LoadSceneAsync(addressableScene);
+    }
+}
